1a542ad630a1ad4401e952fe11bf183d2954837a
Egor Bocharov <dofe71@gmail.com>
2021-12-11 13:36:59 +0300

    test tags & release

--- bot/main.py
+++ bot/main.py
@@ -4,11 +4,15 @@ import os
 
 def printreply(update, context) -> None:
     update.message.reply_text("привет привет")
+
+
 def main():
     updater = Updater(os.environ["MYFIRSTTGBOT"])
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("hello", printreply))
     updater.start_polling()
     updater.idle()
+
+
 if __name__ == "__main__":
     main()
